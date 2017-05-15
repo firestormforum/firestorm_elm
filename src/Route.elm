f5087faa99780c01146dcd7d1b80216801ed20d3
Josh Adams <josh.rubyist@gmail.com>
2017-05-15 16:07:04 -0500

    Add router

---
+++ src/Route.elm
@@ -0,0 +1,28 @@
+module Route exposing (Route(..), fromLocation)
+
+import Navigation exposing (Location)
+import UrlParser as Url exposing (parseHash, s, (</>), string, oneOf, Parser)
+
+
+type Route
+    = Home
+    | Categories
+    | Category String
+    | Thread String String
+
+
+router : Parser (Route -> a) a
+router =
+    oneOf
+        [ Url.map Categories (s "categories")
+        , Url.map Category (s "categories" </> string)
+        , Url.map Thread (s "categories" </> string </> s "threads" </> string)
+        ]
+
+
+fromLocation : Location -> Maybe Route
+fromLocation location =
+    if String.isEmpty location.hash then
+        Just Home
+    else
+        parseHash router location
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -4,6 +4,8 @@ import Test exposing (..)
 import Expect
 import String
 import App
+import Route exposing (Route(..), fromLocation)
+import Navigation exposing (Location)
 
 
 all : Test
@@ -12,4 +14,39 @@ all =
         [ test "Model is initialized properly" <|
             \() ->
                 Expect.equal (Tuple.first <| App.init "") ()
+        , test "no hash -> `Home`" <|
+            \() ->
+                Expect.equal (parseRoute "") (Just Home)
+        , test "`/categories` -> `Categories`" <|
+            \() ->
+                Expect.equal (parseRoute "/categories") (Just Categories)
+        , test "`/categories/foo` -> `Category \"foo\"`" <|
+            \() ->
+                Expect.equal (parseRoute "/categories/foo")
+                    (Just <| Category "foo")
+        , test "`/categories/foo/threads/bar` -> `Thread \"foo\" \"bar\"`" <|
+            \() ->
+                Expect.equal (parseRoute "/categories/foo/threads/bar")
+                    (Just <| Thread "foo" "bar")
         ]
+
+
+parseRoute : String -> Maybe Route
+parseRoute =
+    hash >> fromLocation
+
+
+hash : String -> Location
+hash h =
+    { href = ""
+    , host = ""
+    , hostname = ""
+    , protocol = ""
+    , origin = ""
+    , port_ = ""
+    , pathname = ""
+    , search = ""
+    , hash = h
+    , username = ""
+    , password = ""
+    }
