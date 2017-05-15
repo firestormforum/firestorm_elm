a6ee6ebf439c930848715b7d4c16a62b9e527311
Josh Adams <josh.rubyist@gmail.com>
2017-05-15 13:46:00 -0500

    Make tests pass

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -9,16 +9,7 @@ import App
 all : Test
 all =
     describe "A Test Suite"
-        [ test "App.model.message should be set properly" <|
+        [ test "Model is initialized properly" <|
             \() ->
-                Expect.equal (Tuple.first App.init |> .message) "Your Elm App is working!"
-        , test "Addition" <|
-            \() ->
-                Expect.equal (3 + 7) 10
-        , test "String.left" <|
-            \() ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
-            \() ->
-                Expect.fail "failed as expected!"
+                Expect.equal (Tuple.first <| App.init "") ()
         ]
