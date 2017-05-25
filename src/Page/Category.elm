5cf0fe1048e5d8a379ebf7ea6a4197c04da2f461
Josh Adams <josh.rubyist@gmail.com>
2017-05-24 23:46:05 -0500

    Add list of threads to category page

--- src/Page/Category.elm
+++ src/Page/Category.elm
@@ -2,9 +2,34 @@ module Page.Category exposing (view)
 
 import Html exposing (..)
 import Html.Attributes exposing (class)
-import Data.Category as Category exposing (Category)
+import Data.Category as Category
+import Data.Thread as Thread
+import Route exposing (Route(..))
 
 
-view : Category -> Html msg
+view : Category.Category -> Html msg
 view category =
-    h2 [] [ text category.title ]
+    div [ class "page-category" ]
+        [ h2 [] [ text category.title ]
+        , ol [ class "threads-list" ]
+            (List.map threadView threads)
+        ]
+
+
+threadView : Thread.Thread -> Html msg
+threadView thread =
+    let
+        category =
+            Category.mockCategory
+    in
+        li
+            [ class "thread-item" ]
+            [ a
+                [ Route.href <| Thread (category.slug) thread.slug ]
+                [ text thread.title ]
+            ]
+
+
+threads : List Thread.Thread
+threads =
+    List.repeat 4 Thread.mockThread
