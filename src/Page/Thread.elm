79a99da624e27cc4450a0cc4fcd5cf1c8f36a59a
Josh Adams <josh.rubyist@gmail.com>
2017-05-25 01:51:51 -0500

    Style Page.Thread

--- src/Page/Thread.elm
+++ src/Page/Thread.elm
@@ -1,25 +1,38 @@
 module Page.Thread exposing (view)
 
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, href)
 import Data.Category as Category
 import Data.Thread as Thread
 import Data.Post as Post
-import Route
+import Route exposing (Route(..))
+import Date
 
 
 view : Category.Category -> Thread.Thread -> Html msg
 view category thread =
     div []
-        [ h2 [] [ text thread.title ]
-        , h3 []
-            [ text "from category: "
-            , a
-                [ Route.href <| Route.Category category.slug ]
-                [ text category.title ]
+        [ div [ class "thread-header" ]
+            [ h2 []
+                [ text thread.title ]
+            , div [ class "item-metadata" ]
+                [ a [ href "#", class "username" ]
+                    [ text "@someuser" ]
+                , abbr
+                    [ class "time" ]
+                    [ text <| toString <| Date.fromTime thread.updatedAt ]
+                ]
+            , div [ class "item-metadata" ]
+                [ ul [ class "category-pill" ]
+                    [ li [ class "category -color-20" ]
+                        [ a [ Route.href <| Route.Category category.slug ]
+                            [ text category.title ]
+                        ]
+                    ]
+                ]
             ]
         , ol
-            [ class "posts-list" ]
+            [ class "post-list" ]
             (List.map postView posts)
         ]
 
